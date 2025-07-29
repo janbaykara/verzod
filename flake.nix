09b9dfa752e5436c874152235157649ebb83b56b
Andrew Bastin <andrewbastin.k@gmail.com>
2025-07-29 20:54:35 +0530

    feat: add nix tooling

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A flake for the development of Verzod";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nodejs = pkgs.nodejs_22;
+        pnpm = pkgs.pnpm_10;
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              nodejs
+              pnpm
+            ];
+          };
+        }
+    );
+}
