5963ae7e43b4386dd1510b770ef6e2357aabf966
Andrew Bastin <andrewbastin.k@gmail.com>
2025-07-30 23:16:17 +0530

    chore: enable typecheck on tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     // Only get files from within the src folder
-    include: ['src/**\/*.{test,spec}.?(c|m)[jt]s?(x)']
+    include: ['src/**\/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    typecheck: {
+      enabled: true
+    }
   }
 })
